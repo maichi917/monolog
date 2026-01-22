795377819c16f2327400b7b5d335a341a60ab10c
maichi917 <2525mk.917@gmail.com>
2026-01-22 19:53:20 +0900

    アイテム登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,24 @@ class ItemsController < ApplicationController
   def index
     @items = current_user.items.order(created_at: :desc) # ログイン中のユーザーのアイテムを取得
   end
+
+  def new
+    @item = current_user.items.new # 新しいアイテムを作成
+  end
+
+  def create
+    @item = current_user.items.build(item_params) # ログイン中のユーザーに紐づくアイテムを作成
+    if @item.save
+      redirect_to items_path, success: 'アイテムが作成されました。'
+    else
+    flash.now[:danger] = 'アイテムの作成に失敗しました。'  # ← flash.nowを使う！
+    render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :stock_quantity, :status, :favorite, :memo, :image)
+  end
 end
