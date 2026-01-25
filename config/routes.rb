884009a09fb92d3a5844ea6273fa486ddd30e0af
maichi917 <2525mk.917@gmail.com>
2026-01-25 20:14:54 +0900

    使い切りアイテム一覧の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#top"
 
-  resources :items, only: %i[index new create show edit update destroy]
+  resources :items, only: %i[index new create show edit update destroy] do
+    collection do
+      get :used_up
+      get :in_use
+    end
+  end
 end
