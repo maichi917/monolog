6828b81c2fe4e66379ffa3839fb1a81b902e61f0
maichi917 <2525mk.917@gmail.com>
2026-02-01 12:51:37 +0900

    メールアドレスログインに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items, dependent: :destroy
+
+  # nameは必須（重複は許可）
+  validates :name, presence: true, length: { maximum: 255 }
 end
