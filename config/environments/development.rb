3154a4f0bac1a10c7e2d1290af11aa062a08c124
maichi917 <2525mk.917@gmail.com>
2026-05-20 14:23:05 +0900

    開発環境のパスワードリセットメール実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # 開発環境では送信メールをブラウザで確認する
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
