9e34a1b58e75476808ad6246f9f4813529c44f09
maichi917 <2525mk.917@gmail.com>
2026-05-30 19:45:50 +0900

    Revert "利用規約ページを追加"
    This reverts commit 992325f7332fd515ff276da1e66407bd359641e3.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,17 +1,7 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "利用規約ページを表示できる" do
-    get terms_path
-
-    assert_response :success
-    assert_select "h1", "利用規約"
-  end
-
-  test "フッターから利用規約ページに移動できる" do
-    get root_path
-
-    assert_response :success
-    assert_select "footer a[href='#{terms_path}']", "利用規約"
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
