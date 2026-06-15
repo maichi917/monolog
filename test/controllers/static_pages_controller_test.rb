2b0f05b2612260139c1896d47cf0384357e2302d
maichi917 <2525mk.917@gmail.com>
2026-06-15 16:08:42 +0900

    OGPとX Cardを設定する

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,30 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "トップページにOGPとX Cardを設定している" do
+    get root_path
+
+    assert_response :success
+    assert_select "title", "ものログ"
+    assert_select "meta[name='description'][content]"
+    assert_select "meta[property='og:type'][content='website']"
+    assert_select "meta[property='og:site_name'][content='ものログ']"
+    assert_select "meta[property='og:title'][content='ものログ']"
+    assert_select "meta[property='og:description'][content]"
+    assert_select "meta[property='og:url'][content='http://www.example.com/']"
+    assert_select "meta[property='og:image'][content]"
+    assert_select "meta[property='og:image:width'][content='1200']"
+    assert_select "meta[property='og:image:height'][content='630']"
+    assert_select "meta[property='og:locale'][content='ja_JP']"
+    assert_select "meta[name='twitter:card'][content='summary_large_image']"
+    assert_select "meta[name='twitter:title'][content='ものログ']"
+    assert_select "meta[name='twitter:description'][content]"
+    assert_select "meta[name='twitter:image'][content]"
+
+    og_image = css_select("meta[property='og:image']").first["content"]
+    assert_match %r{\Ahttp://www\.example\.com/assets/ogp(?:-[a-f0-9]+)?\.png\z}, og_image
+  end
+
   test "利用規約ページを表示できる" do
     get terms_path
 
